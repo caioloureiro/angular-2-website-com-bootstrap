a8d75747c1b7681ca701aa0804e4640778515e87
Caio Loureiro <caio.loureiro@hotmail.com>
2020-08-28 18:49:00 -0300

    Add files via upload

---
+++ src/app/quem-somos/quem-somos.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { QuemSomosComponent } from './quem-somos.component';
+import { QuemSomosRoutingModule } from './quem-somos-routing.module';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		QuemSomosRoutingModule,
+	],
+	declarations: [QuemSomosComponent],
+	exports: [QuemSomosComponent],
+})
+export class QuemSomosModule { }
