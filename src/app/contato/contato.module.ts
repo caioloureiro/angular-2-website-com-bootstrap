20b4832fd9435c0c2f076be56a9bc95317b0bdce
Caio Loureiro <caio.loureiro@hotmail.com>
2020-08-28 18:48:01 -0300

    Add files via upload

---
+++ src/app/contato/contato.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato.component';
+import { ContatoRoutingModule } from './contato-routing.module';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		ContatoRoutingModule,
+	],
+	declarations: [ContatoComponent],
+	exports: [ContatoComponent],
+})
+export class ContatoModule {}
